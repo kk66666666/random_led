a9d5cc7566648706d29646f82549dd4a593df8e7
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-12-08 16:46:43 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-
+let list: number[] = []
+let n = 0
+let a = 0
+let x = 0
+let y = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    list = []
+    for (let index = 0; index < 25; index++) {
+        list.push(0)
+    }
+    n = 0
+    while (n < 3) {
+        a = randint(0, 24)
+        if (list[a] == 0) {
+            list[a] = 1
+        }
+        n += 1
+        點燈(a + 1)
+    }
+})
+function 點燈 (num: number) {
+    if (num % 5 != 0) {
+        x = num % 5 - 1
+        y = Math.floor(num) / 5
+    } else {
+        x = 4
+        y = num / 5 - 1
+    }
+    led.plot(x, y)
+}
+basic.forever(function () {
+	
+})

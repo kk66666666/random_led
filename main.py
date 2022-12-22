6d48bf5909a92936bb90f1d7e3d60396330bb857
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-12-22 16:45:28 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -17,8 +17,188 @@ def on_button_pressed_a():
         light_ = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def dic(num: number):
-    pass
+def num_(num: number):
+    global i, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25
+    i = 0
+    _1 = images.create_image("""
+        . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+    """)
+    _2 = images.create_image("""
+        . # # # .
+                . . . # .
+                . # # # .
+                . # . . .
+                . # # # .
+    """)
+    _3 = images.create_image("""
+        . # # # .
+                . . . # .
+                . # # # .
+                . . . # .
+                . # # # .
+    """)
+    _4 = images.create_image("""
+        . # . # .
+                . # . # .
+                . # # # .
+                . . . # .
+                . . . # .
+    """)
+    _5 = images.create_image("""
+        . # # # .
+                . # . . .
+                . # # # .
+                . . . # .
+                . # # # .
+    """)
+    _6 = images.create_image("""
+        . # # # .
+                . # . . .
+                . # # # .
+                . # . # .
+                . # # # .
+    """)
+    _7 = images.create_image("""
+        . # # # .
+                . # . # .
+                . . . # .
+                . . . # .
+                . . . # .
+    """)
+    _8 = images.create_image("""
+        . # # # .
+                . # . # .
+                . # # # .
+                . # . # .
+                . # # # .
+    """)
+    _9 = images.create_image("""
+        . # # # .
+                . # . # .
+                . # # # .
+                . . . # .
+                . # # # .
+    """)
+    _10 = images.create_image("""
+        # . # # #
+                # . # . #
+                # . # . #
+                # . # . #
+                # . # # #
+    """)
+    _11 = images.create_image("""
+        . # . . #
+                . # . . #
+                . # . . #
+                . # . . #
+                . # . . #
+    """)
+    _12 = images.create_image("""
+        # . # # #
+                # . . . #
+                # . # # #
+                # . # . .
+                # . # # #
+    """)
+    _13 = images.create_image("""
+        # . # # #
+                # . . . #
+                # . # # #
+                # . . . #
+                # . # # #
+    """)
+    _14 = images.create_image("""
+        # . # . #
+                # . # . #
+                # . # # #
+                # . . . #
+                # . . . #
+    """)
+    _15 = images.create_image("""
+        # . # # #
+                # . # . .
+                # . # # #
+                # . . . #
+                # . # # #
+    """)
+    _16 = images.create_image("""
+        # . # # #
+                # . # . .
+                # . # # #
+                # . # . #
+                # . # # #
+    """)
+    _17 = images.create_image("""
+        # . # # #
+                # . # . #
+                # . # . #
+                # . . . #
+                # . . . #
+    """)
+    _18 = images.create_image("""
+        # . # # #
+                # . # . #
+                # . # # #
+                # . # . #
+                # . # # #
+    """)
+    _19 = images.create_image("""
+        # . # # #
+                # . # . #
+                # . # # #
+                # . . . #
+                # . # # #
+    """)
+    _20 = images.create_big_image("""
+        . . # # # . # # # .
+                . . . . # . # . # .
+                . . # # # . # . # .
+                . . # . . . # . # .
+                . . # # # . # # # .
+    """)
+    _21 = images.create_big_image("""
+        . . # # # . . # . .
+                . . . . # . . # . .
+                . . # # # . . # . .
+                . . # . . . . # . .
+                . . # # # . . # . .
+    """)
+    _22 = images.create_big_image("""
+        . . # # # . # # # .
+                . . . . # . . . # .
+                . . # # # . # # # .
+                . . # . . . # . . .
+                . . # # # . # # # .
+    """)
+    _23 = images.create_big_image("""
+        . . # # # . # # # .
+                . . . . # . . . # .
+                . . # # # . # # # .
+                . . # . . . . . # .
+                . . # # # . # # # .
+    """)
+    _24 = images.create_big_image("""
+        . . # # # . # . # .
+                . . . . # . # . # .
+                . . # # # . # # # .
+                . . # . . . . . # .
+                . . # # # . . . # .
+    """)
+    _25 = images.create_big_image("""
+        . . # # # . # # # .
+                . . . . # . # . . .
+                . . # # # . # # # .
+                . . # . . . . . # .
+                . . # # # . # # # .
+    """)
+    while False:
+        if True:
+            pass
+        i += 1
 
 def on_button_pressed_ab():
     global light_
@@ -41,6 +221,32 @@ def 點燈(num2: number):
     led.plot(x, y)
 y = 0
 x = 0
+_25: Image = None
+_24: Image = None
+_23: Image = None
+_22: Image = None
+_21: Image = None
+_20: Image = None
+_19: Image = None
+_18: Image = None
+_17: Image = None
+_16: Image = None
+_15: Image = None
+_14: Image = None
+_13: Image = None
+_12: Image = None
+_11: Image = None
+_10: Image = None
+_9: Image = None
+_8: Image = None
+_7: Image = None
+_6: Image = None
+_5: Image = None
+_4: Image = None
+_3: Image = None
+_2: Image = None
+_1: Image = None
+i = 0
 a = 0
 n = 0
 list2: List[number] = []
--- main.ts
+++ main.ts
@@ -20,14 +20,22 @@ input.onButtonPressed(Button.A, function () {
         light_ = 0
     }
 })
-function dic (num: number) {
-	
+function num_ (num: number) {
+    i = 0
+    while (light_ == i) {
+        if (true) {
+            break;
+        }
+        i += 1
+    }
 }
 input.onButtonPressed(Button.AB, function () {
     light_ += -1
+    num_(i)
 })
 input.onButtonPressed(Button.B, function () {
     light_ += 1
+    num_(i)
 })
 function 點燈 (num: number) {
     if (num % 5 == 0) {
@@ -41,11 +49,187 @@ function 點燈 (num: number) {
 }
 let y = 0
 let x = 0
+let i = 0
 let a = 0
 let n = 0
 let list2: number[] = []
 let light_ = 0
 light_ = 0
+let _1 = images.createImage(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+let _2 = images.createImage(`
+    . # # # .
+    . . . # .
+    . # # # .
+    . # . . .
+    . # # # .
+    `)
+let _3 = images.createImage(`
+    . # # # .
+    . . . # .
+    . # # # .
+    . . . # .
+    . # # # .
+    `)
+let _4 = images.createImage(`
+    . # . # .
+    . # . # .
+    . # # # .
+    . . . # .
+    . . . # .
+    `)
+let _5 = images.createImage(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . . . # .
+    . # # # .
+    `)
+let _6 = images.createImage(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+let _7 = images.createImage(`
+    . # # # .
+    . # . # .
+    . . . # .
+    . . . # .
+    . . . # .
+    `)
+let _8 = images.createImage(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+let _9 = images.createImage(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . # .
+    . # # # .
+    `)
+let _10 = images.createImage(`
+    # . # # #
+    # . # . #
+    # . # . #
+    # . # . #
+    # . # # #
+    `)
+let _11 = images.createImage(`
+    . # . . #
+    . # . . #
+    . # . . #
+    . # . . #
+    . # . . #
+    `)
+let _12 = images.createImage(`
+    # . # # #
+    # . . . #
+    # . # # #
+    # . # . .
+    # . # # #
+    `)
+let _13 = images.createImage(`
+    # . # # #
+    # . . . #
+    # . # # #
+    # . . . #
+    # . # # #
+    `)
+let _14 = images.createImage(`
+    # . # . #
+    # . # . #
+    # . # # #
+    # . . . #
+    # . . . #
+    `)
+let _15 = images.createImage(`
+    # . # # #
+    # . # . .
+    # . # # #
+    # . . . #
+    # . # # #
+    `)
+let _16 = images.createImage(`
+    # . # # #
+    # . # . .
+    # . # # #
+    # . # . #
+    # . # # #
+    `)
+let _17 = images.createImage(`
+    # . # # #
+    # . # . #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+let _18 = images.createImage(`
+    # . # # #
+    # . # . #
+    # . # # #
+    # . # . #
+    # . # # #
+    `)
+let _19 = images.createImage(`
+    # . # # #
+    # . # . #
+    # . # # #
+    # . . . #
+    # . # # #
+    `)
+let _20 = images.createBigImage(`
+    . . # # # . # # # .
+    . . . . # . # . # .
+    . . # # # . # . # .
+    . . # . . . # . # .
+    . . # # # . # # # .
+    `)
+let _21 = images.createBigImage(`
+    . . # # # . . # . .
+    . . . . # . . # . .
+    . . # # # . . # . .
+    . . # . . . . # . .
+    . . # # # . . # . .
+    `)
+let _22 = images.createBigImage(`
+    . . # # # . # # # .
+    . . . . # . . . # .
+    . . # # # . # # # .
+    . . # . . . # . . .
+    . . # # # . # # # .
+    `)
+let _23 = images.createBigImage(`
+    . . # # # . # # # .
+    . . . . # . . . # .
+    . . # # # . # # # .
+    . . # . . . . . # .
+    . . # # # . # # # .
+    `)
+let _24 = images.createBigImage(`
+    . . # # # . # . # .
+    . . . . # . # . # .
+    . . # # # . # # # .
+    . . # . . . . . # .
+    . . # # # . . . # .
+    `)
+let _25 = images.createBigImage(`
+    . . # # # . # # # .
+    . . . . # . # . . .
+    . . # # # . # # # .
+    . . # . . . . . # .
+    . . # # # . # # # .
+    `)
 basic.forever(function () {
 	
 })
